7d748e6d79e6d99a959f908c640d5b20c14b1310
ZakariaArc <zakariaa123@hotmail.fr>
2024-04-26 00:47:29 +0100

    Task 4 5 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -6,13 +6,10 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   const photoPromise = uploadPhoto(fileName);
 
   return Promise.allSettled([userPromise, photoPromise]).then((results) => {
-    const formattedResults = results.map((result) => {
-      if (result.status === 'fulfilled') {
-        return { status: result.status, value: result.value };
-      } else {
-        return { status: result.status, value: `Error: ${result.reason.message}` };
-      }
-    });
+    const formattedResults = results.map((result) => ({
+      status: result.status,
+      value: result.status === 'fulfilled' ? result.value : result.reason.message,
+    }));
 
     return formattedResults;
   });
